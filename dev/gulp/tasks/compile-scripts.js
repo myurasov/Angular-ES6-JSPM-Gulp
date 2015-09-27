0d8f60b1aee81abac9ee6dc236face35b146a96f
Mikhail Yurasov <me@yurasov.me>
2015-09-26 18:06:07 -0700

    Move to Babel transpiler

--- dev/gulp/tasks/compile-scripts.js
+++ dev/gulp/tasks/compile-scripts.js
@@ -8,9 +8,11 @@ var utils = require('../utils');
 var config = require('../config');
 
 gulp.task('compile-scripts', (function () {
+
   var command = 'jspm bundle-sfx ' + // create self-sufficient bundle
-                utils.escapeShellArg(config.compileScripts.src) + ' + traceur-runtime ' + // source
+                utils.escapeShellArg(config.compileScripts.src) + ' ' + // source
                 utils.escapeShellArg(config.compileScripts.dest) + // dest
                 ' -m'; // minify
+
   return gulpShell.task([command]);
 })());
