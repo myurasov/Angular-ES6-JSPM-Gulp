87376146d3c7b8ed21067cac3cae4dd8c36dfcd9
Mikhail Yurasov <me@yurasov.me>
2015-09-26 18:23:22 -0700

    Update SystemJS config

---
+++ src/app/scripts/system.config.js
@@ -0,0 +1,29 @@
+System.config({
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "vendor/jspm_packages/github/*",
+    "npm:*": "vendor/jspm_packages/npm/*"
+  },
+
+  map: {
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.2.0",
+    "lodash": "npm:lodash@3.10.1",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:babel-runtime@5.8.25": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.2.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:lodash@3.10.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
